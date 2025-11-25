d3fc1a2154afd45ff3342c40cc267efdae1ace89
Hoochymoochy <Khaydenroberts22@gmail.com>
2025-11-24 23:35:49 -0700

    setup mutli router

---
+++ i18n.ts
@@ -0,0 +1,8 @@
+import { defineRouting } from 'next-intl/routing';
+
+export const routing = defineRouting({
+  locales: ['en', 'pt'],
+  defaultLocale: 'en'
+});
+
+export const { locales, defaultLocale } = routing;
